7759090e01ddc9720281e1a1b3553b023c45c617
Ángel Inda <idv19c.ainda@uartesdigitales.edu.mx>
2020-02-01 06:14:02 -0600

    Create folders and add files for UI implementation

---
+++ Cells Alive/Assets/Scripts/UI/CamaraShake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraShake : MonoBehaviour
+{
+	bool boolShake = false;
+	Vector3 cameraInitialPosition;
+	public float shakeMagnetude = 0.05f, shakeTime = 0.5f;
+	public Camera mainCamera;
+
+	public void ShakeIt()
+	{
+		cameraInitialPosition = mainCamera.transform.position;
+		InvokeRepeating("StartCameraShaking", 0f, 0.005f);
+		Invoke("StopCameraShaking", shakeTime);
+	}
+
+	public void StartCameraShaking()
+	{
+		float cameraShakingOffsetX = Random.value * shakeMagnetude * 2 - shakeMagnetude;
+		float cameraShakingOffsetY = Random.value * shakeMagnetude * 2 - shakeMagnetude;
+		Vector3 cameraIntermadiatePosition = mainCamera.transform.position;
+		cameraIntermadiatePosition.x += cameraShakingOffsetX;
+		cameraIntermadiatePosition.y += cameraShakingOffsetY;
+		mainCamera.transform.position = cameraIntermadiatePosition;
+	}
+
+	public void StopCameraShaking()
+	{
+		CancelInvoke("StartCameraShaking");
+		mainCamera.transform.position = cameraInitialPosition;
+	}
+
+
+	void Update()
+	{
+		if(boolShake == true)
+		{
+			ShakeIt();
+			boolShake = false;
+		}
+	}
+}
---
+++ Cells Alive/Assets/Scripts/UI/ExitCredits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitCredits : MonoBehaviour
+{
+    float Tiempo = 0;
+    void Update()
+    {
+        Tiempo += Time.deltaTime;
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length <= Tiempo)
+            SceneManager.LoadScene("Menu");
+    }
+}
