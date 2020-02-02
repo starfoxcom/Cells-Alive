0aa75c613195d12f18acac59be0c28f2cda0bdfc
Jesus Alberto del Moral Cupil <idv18c.jmoral@uartesdigitales.edu.mx>
2020-02-02 07:08:03 -0600

    Sound adition for game

---
+++ Cells Alive/Assets/Scripts/Sounds/Sounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour {
+    // Start is called before the first frame update
+
+    public AudioClip Main;
+    public AudioClip Button;
+    public AudioClip MainShoot;
+    public AudioClip SecondShoot;
+
+    public AudioSource source;
+
+    void Start() {
+        source = GetComponent<AudioSource>();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void buttonSound() {
+        source.Play();
+    }
+
+}
